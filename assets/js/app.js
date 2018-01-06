96d021c67dba9986724e2ceb17b6e0bff5338857
Audra Mook <asmook87@hotamil.com>
2018-01-06 09:42:39 -0700

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,30 @@
 // Let the fun begin!
+var topics = ["dog","cat","monkey","frog","elephant","fish","bird"];
+var testTopic = "tarantula";
+
+
+
+function createButtons() {
+	for (var i = 0; i<topics.length; i++){
+		var button = $("<button>");
+		button.text(topics[i]);
+		$(".buttons").append(button);
+	}
+
+}
+
+createButtons();
+
+$(".buttons").on("click", "button", function(){
+	var chosenAnimal = $(this).text();
+	var QueryString = "http://api.giphy.com/v1/gifs/search?q=" + chosenAnimal +"&limit=5&api_key=dc6zaTOxFJmzC";
+
+	$.ajax({
+		method: "GET",
+		url: QueryString
+	}).done(function(response) {
+		console.log(response);
+
+	})
+
+});
