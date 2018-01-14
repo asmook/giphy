41b54fe1c7b8b1b278bb00dc24487eb63a521b9a
Audra Mook <asmook87@hotamil.com>
2018-01-14 15:32:15 -0700

    final commit for turn in

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,15 +16,32 @@ function createButtons() {
 createButtons();
 
 $(".buttons").on("click", "button", function(){
+	$(".coolGifs").empty();
 	var chosenAnimal = $(this).text();
-	var QueryString = "http://api.giphy.com/v1/gifs/search?q=" + chosenAnimal +"&limit=5&api_key=dc6zaTOxFJmzC";
+	var QueryString = "http://api.giphy.com/v1/gifs/search?q=" + chosenAnimal +"&limit=5&rating=g&api_key=dc6zaTOxFJmzC";
 
 	$.ajax({
 		method: "GET",
 		url: QueryString
 	}).done(function(response) {
-		console.log(response);
+		 console.log(response);
+		//i=0(this is where the loop should start
+		// i< response.data.length is where the loop should end
+		// i++ is how we get from point a to point b)
+		for (i = 0; i < response.data.length; i++){	
+			//takes in a string phrase and converts it to an interger
+			//we had to do this bc otherwise js thought that i+1 should be interpereted as a string 
+			//ex instead of 1 2 3 4 5 it gane me 01 11 21 31 41 
+			console.log("hi there" + parseInt(i+1));
 
-	})
+			var img = $("<img>");
+			//figure this shit out
+			img.attr("src", response.data[i].images.fixed_height.url);
+			$(".coolGifs").append(img);
+		
+		}
+
+
+	});
 
 });
